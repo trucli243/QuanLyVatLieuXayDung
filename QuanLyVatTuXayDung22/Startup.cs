aa6ca620a8b0e44f1afc11019031fb3ac2dde670
nguyenthanhhien1502 <nguyenthanhhien15202@gmail.com>
2023-08-18 22:51:35 +0700

    tao class

--- QuanLyVatTuXayDung22/Startup.cs
+++ QuanLyVatTuXayDung22/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +27,47 @@ namespace QuanLyVatTuXayDung22
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            #region -- Swagger --  
+            var inf1 = new OpenApiInfo
+            {
+                Title = "API v1.0",
+                Version = "v1",
+                Description = "Swashbuckle",
+                TermsOfService = new Uri("http://appointvn.com"),
+                Contact = new OpenApiContact
+                {
+                    Name = "Thao Hien",
+                    Email = "nguyenthanhhien15202@gmail.com"
+                },
+                License = new OpenApiLicense
+                {
+                    Name = "Apache 2.0",
+                    Url = new Uri("http://www.apache.org/licenses/LICENSE-2.0.html")
+                }
+            };
+            var inf2 = new OpenApiInfo
+            {
+                Title = "API v2.0",
+                Version = "v2",
+                Description = "Swashbuckle",
+                TermsOfService = new Uri("http://appointvn.com"),
+                Contact = new OpenApiContact
+                {
+                    Name = "Thao Hien",
+                    Email = "nguyenthanhhien15202@gmail.com"
+                },
+                License = new OpenApiLicense
+                {
+                    Name = "Apache 2.0",
+                    Url = new Uri("http://www.apache.org/licenses/LICENSE-2.0.html")
+                }
+            };
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", inf1);
+                c.SwaggerDoc("v2", inf2);
+            });
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +77,15 @@ namespace QuanLyVatTuXayDung22
             {
                 app.UseDeveloperExceptionPage();
             }
+            #region -- Swagger --
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1.0");
+                c.SwaggerEndpoint("/swagger/v2/swagger.json", "API v2.0");
+            });
+            #endregion
+            app.UseCors(p => p.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 
             app.UseHttpsRedirection();
 
